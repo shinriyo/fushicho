ece0214c16414dfe3e5a6caba0d0effbc36f4e06
shinriyo <shinriyo@shinriyos-MacBook.local>
2016-08-27 16:05:08 +0900

    create dir and finish

--- test/fushicho_test.exs
+++ test/fushicho_test.exs
@@ -4,9 +4,25 @@ defmodule FushichoTest do
 
   setup_all do
     IO.puts "now setup_all"
+
+   # テスト用フォルダ
+    File.mkdir "web"
+    File.mkdir "web/static"
+    File.mkdir "web/static/js"
+
+    on_exit fn ->
+      IO.puts "finish"
+      # 後始末
+      File.rm_rf "web"
+    end
   end
 
-  test "the truth" do
+  test "check Phoenix project" do
+      IO.puts "nonono"
+    assert Fushicho.sum(1, 1) == 2
+  end
+
+  test "check scaffolding" do
     assert Fushicho.sum(1, 1) == 2
   end
 end
