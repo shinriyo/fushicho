9ebb12d5d296e85871995de212e49be33531528a
shinriyo <shinriyo@shinriyos-MacBook.local>
2016-09-11 10:09:13 +0900

    temp react js

--- lib/fushicho.ex
+++ lib/fushicho.ex
@@ -213,15 +213,18 @@ defmodule Mix.Tasks.Fushicho do
         field_name_cap = Regex.scan(field_name_reg, contain)
         field_list  = Enum.map(field_name_cap, fn(x) -> Enum.at(x, 1)  end)
         # これが実際のモデルのフィールドに相当する
-        IO.inspect field_list
+        #IO.inspect field_list
 
         field_type_reg = ~r/.*field :.*, :(.*)/
         field_type_cap = Regex.scan(field_type_reg, contain)
         type_list  = Enum.map(field_type_cap, fn(x) -> Enum.at(x, 1)  end)
         # これが実際のモデルのフィールドの型に相当する
-        IO.inspect type_list
+        #IO.inspect type_list
+        type_list
+        |> Enum.with_index
+        |> Enum.each(fn(x) -> IO.inspect elem(x, 1) end)
 
-        # 実際にjs
+        # Reactのjsを生成
         path = "web/static/js/"
         filename = path <> name <> ".js"
         # ファイル開く
@@ -231,18 +234,293 @@ defmodule Mix.Tasks.Fushicho do
         # ~sの部分が置換される
         capitalized = String.capitalize(name)
         contain = """
-        import React from 'react'
-        import ReactDOM from 'react-dom'
-        import request from 'superagent';
+        import React from 'react';
+        var ReactDOM = require('react-dom');
+        import $ from 'jquery';
 
-        class ~s extends React.Component {
-        ~s
-        }
+        var SearchPanel = React.createClass({
+            render: function() {
+                return (
+                    <div className="row">
+                        <div className="one-fourth column">
+                            Filter: &nbsp;
+                            <input ref='search' type='text' value={this.props.search} onChange={this.onSearchChanged} />
+                            {this.props.search?<button onClick={this.props.onClearSearch} >x</button>:null}
+                        </div>
+                    </div>
+                )
+            },
+            onSearchChanged: function() {
+                var query = ReactDOM.findDOMNode(this.refs.search).value;
+                this.props.onSearchChanged(query);
+            }
+        });
+
+        var ~sTableRow = React.createClass({
+            render: function() {
+                return (
+                    <tr>
+                        <td>{this.props.book.title}</td>
+                        <td>{this.props.book.category}</td>
+                        <td><a href='#' onClick={this.onClick}>Edit</a></td>
+                    </tr>
+                );
+            },
+            onClick: function(id) {
+                this.props.handleEditClickPanel(this.props.~s.id);
+            }
+        });
+
+        var BookTable = React.createClass({
+            render: function() {
+                var rows = [];
+                for(var i = 0; i < this.props.books.length; i++) {
+                    var book = this.props.books[i];
+                    rows.push(<BookTableRow key={book.id} book={book} handleEditClickPanel={this.props.handleEditClickPanel}  />);
+                }
+                // なぜかforEachできない
+                // this.props.books.forEach(function(book) {
+                //     rows.push(<BookTableRow key={book.id} book={book} handleEditClickPanel={this.props.handleEditClickPanel}  />);
+                // }.bind(this));
+                return (
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Category</th>
+                                <th>Edit</th>
+                            </tr>
+                        </thead>
+                        <tbody>{rows}</tbody>
+                    </table>
+                );
+            }
+        });
+
+        var BookForm = React.createClass({
+            render: function() {
+                return(
+                    <form onSubmit={this.props.handleSubmitClick}>
+                        <label forHtml='title'>Title</label><input ref='title' name='title' type='text' value={this.props.book.title} onChange={this.onChange}/>
+                        <label forHtml='category'>Category</label>
+                        <select ref='category' name='category' value={this.props.book.category} onChange={this.onChange} >
+                            <option value='CRIME' >Crime</option>
+                            <option value='HISTORY'>History</option>
+                            <option value='HORROR'>Horror</option>
+                            <option value='SCIFI'>SciFi</option>
+                        </select>
+                        <br />
+                        <input type='submit' value={this.props.book.id?"Save (id = " +this.props.book.id+ ")":"Add"} />
+                        {this.props.book.id?<button onClick={this.props.handleDeleteClick}>Delete</button>:null}
+                        {this.props.book.id?<button onClick={this.props.handleCancelClick}>Cancel</button>:null}
+                        {this.props.message?<div>{this.props.message}</div>:null}
+                    </form>
+                );
+            },
+            onChange: function() {
+                var title = ReactDOM.findDOMNode(this.refs.title).value;
+                var category = ReactDOM.findDOMNode(this.refs.category).value;
+                this.props.handleChange(title, category);
+            }
+        });
+
+        var BookPanel = React.createClass({
+            getInitialState: function() {
+                return {
+                    books: [],
+                    editingBook: {
+                        title:"",
+                        category:"",
+                    },
+                    search:"",
+                    message:""
+                };
+            },
+            render: function() {
+                return(
+                    <div className="row">
+                        <div className="one-half column">
+                            <SearchPanel
+                                search={this.state.search}
+                                onSearchChanged={this.onSearchChanged}
+                                onClearSearch={this.onClearSearch}
+                            />
+                            <BookTable books={this.state.books} handleEditClickPanel={this.handleEditClickPanel} />
+                        </div>
+                        <div className="one-half column">
+                            <BookForm 
+                                book={this.state.editingBook} 
+                                message={this.state.message} 
+                                handleChange={this.handleChange}
+                                handleSubmitClick={this.handleSubmitClick}
+                                handleCancelClick={this.handleCancelClick}
+                                handleDeleteClick={this.handleDeleteClick}
+                            />
+                        </div>
+                    </div>
+                );
+            },
+            componentDidMount: function() {
+                this.reloadBooks('');
+            },
+            onSearchChanged: function(query) {
+                if (this.promise) {
+                    clearInterval(this.promise)
+                }
+                this.setState({
+                    search: query
+                });
+                this.promise = setTimeout(function () {
+                    this.reloadBooks(query);
+                }.bind(this), 200);
+            },
+            onClearSearch: function() {
+                this.setState({
+                    search: ''
+                });
+                this.reloadBooks('');
+            },
+            // Edit押下した瞬間
+            handleEditClickPanel: function(id) {
+                var book = $.extend({}, this.state.books.filter(function(x) {
+                    return x.id == id;
+                })[0] );
+                
+                this.setState({
+                    editingBook: book,
+                    message: ''
+                });
+            },
+            // 入力した瞬間
+            handleChange: function(title, category) {
+                this.setState({
+                    editingBook: {
+                        title: title,
+                        category: category,
+                        id: this.state.editingBook.id
+                    }
+                });
+            },
+            handleCancelClick: function(e) {
+                this.setState({
+                    editingBook: {}
+                });
+            },    
+            reloadBooks: function(query) {
+                $.ajax({
+                    // url: this.props.url+'?search='+query,
+                    url: this.props.url,
+                    dataType: 'json',
+                    // これら必須
+                    contentType: 'application/json',
+                    beforeSend: function(req) {
+                        req.setRequestHeader('Accept', 'application/json');
+                    },
+                    cache: false,
+                    success: function(data) {
+                        this.setState({
+                            books: data.data.reverse(),
+                            search: query
+                        });
+                    }.bind(this),
+                    error: function(xhr, status, err) {
+                        console.error(this.props.url, status, err.toString());
+                        this.setState({
+                            message: err.toString()
+                        });
+                    }.bind(this)
+                });
+            },
+            handleSubmitClick: function(e) {
+                e.preventDefault();
+                if(this.state.editingBook.id) {
+                    // 編集
+                    $.ajax({
+                        url: this.props.url+this.state.editingBook.id,
+                        dataType: 'json',
+                        method: 'PUT',
+                        // これら必須
+                        contentType: 'application/json',
+                        beforeSend: function(req) {
+                            req.setRequestHeader('Accept', 'application/json');
+                        },
+                        // data:this.state.editingBook,
+                        data: JSON.stringify({book:this.state.editingBook}),
+                        cache: false,
+                        success: function(data) {
+                            this.setState({
+                                message: "Successfully updated book!"
+                            });
+                            this.reloadBooks('');
+                        }.bind(this),
+                        error: function(xhr, status, err) {
+                            console.error(this.props.url, status, err.toString());
+                            this.setState({
+                                message: err.toString()
+                            });
+                        }.bind(this)
+                    });
+                } else {
+                    // 新規
+                    $.ajax({
+                        url: this.props.url,
+                        dataType: 'json',
+                        method: 'POST',
+                        // data:this.state.editingBook,
+                        data: JSON.stringify({book:this.state.editingBook}),
+                        // これら必須
+                        contentType: 'application/json',
+                        beforeSend: function(req) {
+                            req.setRequestHeader('Accept', 'application/json');
+                        },
+                        cache: false,
+                        success: function(data) {
+                            this.setState({
+                                message: "Successfully added book!"
+                            });
+                            this.reloadBooks('');
+                        }.bind(this),
+                        error: function(xhr, status, err) {
+                            console.error(this.props.url, status, err.toString());
+                            this.setState({
+                                message: err.toString()
+                            });
+                        }.bind(this)
+                    });
+                }
+                this.setState({
+                    editingBook: {}
+                });
+            },
+            handleDeleteClick: function(e) {
+                e.preventDefault();
+                $.ajax({
+                    url: this.props.url+this.state.editingBook.id,
+                    method: 'DELETE',
+                    cache: false,
+                    success: function(data) {
+                        this.setState({
+                            message: "Successfully deleted book!",
+                            editingBook: {}
+                        });
+                        this.reloadBooks('');
+                    }.bind(this),
+                    error: function(xhr, status, err) {
+                        console.error(this.props.url, status, err.toString());
+                        this.setState({
+                            message: err.toString()
+                        });
+                    }.bind(this)
+                });
+            },
+        });
+
+        ReactDOM.render(<BookPanel url='/api/books/' />, document.getElementById('content'));
         """
         # 修正
         fix = :io_lib.format(contain, [capitalized, name])
-        # IO.binwrite file, fix
-        IO.puts(fix)
+        IO.binwrite file, fix
+        # IO.puts(fix)
         true
      end
     end
